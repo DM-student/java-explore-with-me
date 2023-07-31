778fbe0dbdbc9c62a5c9406051080b7a0ab40429
DM <dima.markov2003@gmail.com>
2023-08-01 02:46:46 +0300

    ошибка 400 для статистики

--- stats/stats-server/src/main/java/ru/practicum/stats/server/controllers/ErrorHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/controllers/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.stats.server.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,7 +15,9 @@ public class ErrorHandler {
     public ResponseEntity<Map<String, String>> getResponseEntity(Throwable e) {
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        // Тут возможно будет проверка на классы ошибок, чтобы менять код ответа.
+        if (e.getClass() == MissingServletRequestParameterException.class) {
+            status = HttpStatus.BAD_REQUEST;
+        }
 
         Map<String, String> response = new HashMap<>();
         response.put("error", e.getMessage());
